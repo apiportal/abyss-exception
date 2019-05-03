1b5e05d18e97743d3f89d29c4cde15f086f504e1
halil.ozkan <halil.ozkan@verapi.com>
2019-05-03 15:53:06 +0300

    ABYSSP-431 many changes to deploy into Github repo
    	unnecessary and inherited maven elements removed
    	github issue templates and related md files added
    	source code copyrights updated to Apache 2.0

--- src/main/java/com/verapi/abyss/exception/AbyssApiException.java
+++ src/main/java/com/verapi/abyss/exception/AbyssApiException.java
@@ -1,22 +1,29 @@
 /*
  *
- *  *  Copyright (C) Verapi Yazilim Teknolojileri A.S. - All Rights Reserved
+ *  * Licensed to the Apache Software Foundation (ASF) under one
+ *  * or more contributor license agreements.  See the NOTICE file
+ *  * distributed with this work for additional information
+ *  * regarding copyright ownership.  The ASF licenses this file
+ *  * to you under the Apache License, Version 2.0 (the
+ *  * "License"); you may not use this file except in compliance
+ *  * with the License.  You may obtain a copy of the License at
  *  *
- *  *  Unauthorized copying of this file, via any medium is strictly prohibited
- *  *  Proprietary and confidential
+ *  *   http://www.apache.org/licenses/LICENSE-2.0
  *  *
- *  *  Written by Halil Özkan <halil.ozkan@verapi.com>, 5 2018
+ *  * Unless required by applicable law or agreed to in writing,
+ *  * software distributed under the License is distributed on an
+ *  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  * KIND, either express or implied.  See the License for the
+ *  * specific language governing permissions and limitations
+ *  * under the License.
  *
  */
 
 package com.verapi.abyss.exception;
 
 import io.netty.handler.codec.http.HttpResponseStatus;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class AbyssApiException extends AbyssException {
-    private static Logger logger = LoggerFactory.getLogger(AbyssApiException.class);
 
     ApiSchemaError apiSchemaError;
     HttpResponseStatus httpResponseStatus; //http://www.restapitutorial.com/httpstatuscodes.html
@@ -39,7 +46,6 @@ public class AbyssApiException extends AbyssException {
 
     public AbyssApiException(ApiSchemaError apiSchemaError) {
         super(apiSchemaError.toString());
-//        logger.error(this.getClass().getName(), apiSchemaError);
         this.apiSchemaError = apiSchemaError;
     }
 
